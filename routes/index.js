7920de23c6858715207bc8c91439f34f3a8e3b7c
dexterb <dexter_baga@yahoo.com>
2017-01-03 23:33:31 -0800

    refactored data provider to come from an api endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data = [
-      {Name:"Dexter Baga",Phone:"619-001-4562"},
-      {Name:"Kent Johnson",Phone:"760-331-4562"},
-      {Name:"Julie Parks",Phone:"918-001-2312"}
-  ]
-  res.render('index', { title: 'My Phone Book App', list:data });
+    var url = 'https://4xfyet0w49.execute-api.us-east-1.amazonaws.com/api/list';
+    var request = https.get(url, function (response) {
+        response.setEncoding('utf8');
+        response.on('data',function (data) {
+            if(data.length > 0){
+                res.render('index', { title: 'My Phone Book App', list:JSON.parse(data) });
+            }else{
+                res.render('index', { title: 'My Phone Book App', list:{} });
+            }
+        })
+    })
 });
 
 module.exports = router;
